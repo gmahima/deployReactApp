d596712d096df09bb868e3c4a26fac936113f121
mahima gangavarapu <mahimagangavarapu@gmail.com>
2019-03-30 07:10:03 +0530

    Updates

---
+++ precache-manifest.860d338212ccf15900bbdb05dd41ddb6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "d30058cdf4828bf42435",
+    "url": "/deployReactApp/static/js/runtime~main.d30058cd.js"
+  },
+  {
+    "revision": "1abda2997865fef5045e",
+    "url": "/deployReactApp/static/js/main.1abda299.chunk.js"
+  },
+  {
+    "revision": "f5ae2494a8dbd9f95f4b",
+    "url": "/deployReactApp/static/js/1.f5ae2494.chunk.js"
+  },
+  {
+    "revision": "1abda2997865fef5045e",
+    "url": "/deployReactApp/static/css/main.6770951d.chunk.css"
+  },
+  {
+    "revision": "671deca009858f5e2695a551724f754a",
+    "url": "/deployReactApp/index.html"
+  }
+];
